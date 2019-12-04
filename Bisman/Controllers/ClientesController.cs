c71dfc095e5e232b84bb31645251c5790aef4dbf
Helio Silva <suporte.computek@gmail.com>
2019-12-04 01:09:22 -0200

    Controller Clientes

---
+++ Bisman/Controllers/ClientesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Bisman.Models;
+using Bisman.Services;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Bisman.Controllers
+{
+
+    public class ClientesController : Controller
+    {
+      
+    }
+}
