3338768032f626018ea6cf1b679a8a4cd95b30ef
Helio Silva <suporte.computek@gmail.com>
2019-12-04 01:07:45 -0200

    Fix Model Cliente

--- Bisman/Models/Cliente.cs
+++ Bisman/Models/Cliente.cs
@@ -7,7 +7,32 @@ namespace Bisman.Models
 {
     public class Cliente
     {
+        public int Id { get; set; }
 
+        [Required(ErrorMessage = "Preencha o campo {0}")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "O Tamanho do {0} deve ser entre {2} e {1} caracteres")]
+        public string Nome { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Endereco { get; set; }
+        [Required(ErrorMessage = "Preencha o campo {0}")]
+        [EmailAddress(ErrorMessage = "Digite um Email Válido")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+   
+
+        public Cliente()
+        {
+        }
+
+        public Cliente(int id, string nome, string endereco, string email)
+        {
+            Id = id;
+            Nome = nome;
+            Endereco = endereco;
+            Email = email;
+            Senha = senha;
+        }
               
         
     }
