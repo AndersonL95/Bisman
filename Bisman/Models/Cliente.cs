2a49cef970abe6034c1fb943536936e5668a0fbf
Helio Silva <suporte.computek@gmail.com>
2019-12-04 01:02:55 -0200

    Adicionado Model Cliente

---
+++ Bisman/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Bisman.Models
+{
+    public class Cliente
+    {
+
+              
+        
+    }
+}
